4e8da2f7a85fb99f3249cdab76bce3ee4ff398c5
YoucefKF <kfyoucef@gmail.com>
2021-11-01 01:24:31 +0100

    Premiers essais de création de l'interface SWING

---
+++ src/vue/LoginForm.java
@@ -0,0 +1,44 @@
+package vue;
+
+import javax.swing.*;  
+import java.awt.*;  
+import java.awt.event.*;  
+ 
+
+public class LoginForm extends JFrame implements ActionListener {
+
+	JButton b1;
+	JPanel newPanel;
+	JLabel userLabel, passLabel;
+	final JTextField TextField1, textField2;
+	
+	
+	public LoginForm() {
+		
+		//Create label for user name
+		userLabel = new JLabel();
+		userLabel.setText("Username");
+		
+		
+	}
+	
+	
+	
+	
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	
+
+	
+
+}
--- src/vue/UserInterfaceSWING.java
+++ src/vue/UserInterfaceSWING.java
@@ -1,40 +1,59 @@
 package vue;
 
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 import javax.swing.plaf.DimensionUIResource;
+import javax.swing.plaf.nimbus.NimbusLookAndFeel;
 
 public class UserInterfaceSWING extends JFrame {
 	
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
+	
+	private JButton btnAjouter;
+	private JButton supprimer;
+	private JPanel contentPane;
+	private JCheckBox chkCheckMe;
+	JTextField txtEditMe;
 	
 	public UserInterfaceSWING() {
 		super( "Transporting company application" );
-        this.setDefaultCloseOperation( JFrame.DISPOSE_ON_CLOSE );
-        this.setSize( 600, 400 );
-        this.setLocationRelativeTo( null );
-        JPanel contentPane = (JPanel) this.getContentPane();
-        JButton btnPushMe = new JButton( "Push me!" );
-        contentPane.add( btnPushMe );
-        JButton btnClickMe = new JButton( "Click me!!!!!!!!!!" );
-        contentPane.add( btnClickMe );
-        JCheckBox chkCheckMe = new JCheckBox( "Check me!" );
-        contentPane.add( chkCheckMe );
-        JTextField txtEditMe = new JTextField( "Edit me!" );
-        txtEditMe.setPreferredSize( new DimensionUIResource(500, 300));
+		
+		this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        this.setSize(new DimensionUIResource(200, 300));
+        this.setLocationRelativeTo(null);
+        
+        this.contentPane = (JPanel) this.getContentPane();
+        this.setLayout(new FlowLayout());
+        
+        
+        this.btnAjouter = new JButton ("Ajouter ");
+        contentPane.add(btnAjouter);
+        
+        this.supprimer = new JButton("Supprimer"); 
+        contentPane.add(this.supprimer);   
+        
+        this.chkCheckMe = new JCheckBox( "Check me!" );
+        contentPane.add(chkCheckMe );
+        
+        this.txtEditMe = new JTextField( "Edit me!" );
+        txtEditMe.setPreferredSize( new Dimension( 120,  30 ) );
         contentPane.add( txtEditMe );
+        
 	}
-	
-	
 
-	public static void main(String[] args) {
+
+	public static void main(String[] args) throws UnsupportedLookAndFeelException {
 		// Start my window
+		
+		UIManager.setLookAndFeel(new NimbusLookAndFeel());
 		UserInterfaceSWING  myWindow = new UserInterfaceSWING ();
         myWindow.setVisible( true );
 		        
